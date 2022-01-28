9c2dbd1f2d8753daacc2835a59e6ac07a4a1c78c
Mattia Bardino <Mattia.errol@gmail.com>
2022-01-28 15:14:57 +0100

    .

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-const images = [
+const items = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
     'img/04.jpg',
-    'img/05.jpg',
+    'img/05.jpg'
 ];
 
-let pictures = "";
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
 
-for (i=0; i < images.length; i++); {
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+let slides = "";
+
+let i = "";
+
+for (i=0; i < items.length; i++); {
     
-    pictures += `<div class="thumb-big-card active">
-    <img class="cover" src="${images[i]}">
-    </div>`;
-
+    slides += `<div class="thumb-big-card active">
+                   <img class="cover" src="${items[i]}">
+               </div>`;
 }
 
 let picturesContainer = document.querySelector(".pictures-container");
-picturesContainer.innerHTML = (pictures); 
+picturesContainer.innerHTML = slides; 
+
 
 
 
